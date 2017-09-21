0ea939f968f2feb52b621f403b44201e8e182733
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-09-21 12:33:09 +0000

    Code refactor and cleanup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,8 +5,9 @@
  */
 
 
-// More efficient method in which an existing HTML template is
-// used and returned
+// Function returning an HTML template for a tweet element.
+// The information pertaining to each tweet is provided
+// by helper functions below
 function createTweetElement1() {
 
   var tweetHTML = `<article class="tweet">
@@ -28,9 +29,9 @@ function createTweetElement1() {
   return tweetHTML;
 }
 
-
+// Will accept arry of tweet objects
+// and transfer relevant info to HTML template
 function renderTweets(tweetsArray) {
-  // var tweetsArray = loadTweets(url);
   for (var tweet of tweetsArray) {
     $tweet = createTweetElement1(tweet);
     $('#tweets-container').prepend($tweet);
@@ -42,20 +43,50 @@ function renderTweets(tweetsArray) {
   }
 }
 
+// Will use jQuery to make requests to /tweets
+// and receive JSON. Will also transfer JSON to renderTweets
 function loadTweets() {
-  // Will use jQuery to make requests to /tweets
-  // and receive JSON
-
   $.get("/tweets").done(function(data) {
     renderTweets(data);
   });
 }
 
+// Helper function to toggle New Tweet area
+function toggleNewTweet() {
+  $(".new-tweet").slideToggle();
+  $(".new-tweet").find("textarea").focus();
+}
+
+// Helper function for form validation
+function takeInputText(input) {
+  var $form = $('#new-tweet-submit');
+  if (input === '') {
+    $("#error-message").empty();
+    $("#error-message").text("You have not typed anything to post!");
+  } else if (input.length > 140) {
+    $("#error-message").empty();
+    $("#error-message").text("Your post is too long, please keep it to 140 characters or below.");
+  } else {
+    $.post("/tweets/", $form.serialize())
+    .done(function(data) {
+      console.log('Button clicked, performing ajax call...');
+      $("#error-message").empty();
+      $(document).find("#tweets-container").empty();
+      loadTweets();
+    }).fail(function(error) {
+      console.error(error);
+    })
+  }
+}
 
 $(document).ready(function () {
   loadTweets();
-  $("#compose-button").on("click", function() {
-    $(".new-tweet").slideToggle();
-    $(".new-tweet").find("textarea").focus();
+  $("#compose-button").on("click", toggleNewTweet);
+  var $form = $('#new-tweet-submit');
+  $form.on('submit', function (event) {
+    event.preventDefault();
+    var formData = $(this).serialize();
+    var inputValue = $("#new-tweet-input").val();
+    takeInputText(inputValue);
   });
 });// to add it to the page so we can make sure it's got all the right elements, classes, etc.
